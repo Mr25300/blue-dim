a88eb18d35d92539f1fd67916cc449e282baeb12
Sebastian Cooper-Giannotta <sebastian.cooper29@gmail.com>
2025-07-19 14:25:57 -0400

    converting to OOP and cleaning up code

--- main.py
+++ main.py
@@ -9,113 +9,159 @@ import ctypes.util
 import time
 import math
 
-NEUTRAL_TEMP = 6500
-SHIFT_START_ANGLE = 15
-SHIFT_END_ANGLE = -5
-
-day_temp = 6500 # natural white light
-night_temp = 3000 # orange colour
-night_dim_percent = 0.4
-
 def clamp(n, minimum, maximum):
     return max(minimum, min(maximum, n))
 
-def get_rgb_from_temp(temperature):
-    temperature /= 100
+class LocationHandler:
+    location: LocationInfo
 
-    if temperature <= 66:
-        red = 255
-    else:
-        red = 329.698727446 * ((temperature - 60) ** -0.1332047592)
-        red = clamp(red, 0, 255)
-
-    if temperature <= 66:
-        green = 99.4708025861 * math.log(temperature) - 161.1195681661
-    else:
-        green = 288.1221695283 * ((temperature - 60) ** -0.0755148492)
-        green = clamp(green, 0, 255)
+    def __init__(self):
+        self.set_manual_location(0, 0)
     
-    if temperature >= 66:
-        blue = 255
-    elif temperature <= 19:
-        blue = 0
-    else:
-        blue = 138.5177312231 * math.log(temperature - 10) - 305.0447927307
-        blue = clamp(blue, 0, 255)
+    def set_manual_location(self, lat, lon):
+        self.location = LocationInfo(latitude=lat, longitude=lon)
+    
+    def update_location(self):
+        data = requests.get("https://ipinfo.io/json").json()
+        lat, lon = map(float, data["loc"].split(","))
 
-    return (red / 255, green / 255, blue / 255)
+        self.set_manual_location(lat, lon)
 
-def get_sun_elevation_angle(lat, lon, time):
-    location = LocationInfo(latitude=lat, longitude=lon)
-    elevation_angle = 90 - zenith(location.observer, time, True)
+class TempHandler:
+    day_temp: float
+    night_temp: float
+    dim_percent: float
+    brightness_gamma: float
 
-    return elevation_angle
+    def __init__(self):
+        self.day_temp = 6500
+        self.night_temp = 3000
+        self.dim_percent = 0.3
+        self.brightness_gamma = 2.2
+    
+    def get_temp_colour(self, night_shift):
+        temperature = self.day_temp + (self.night_temp - self.day_temp) * night_shift
+        temperature /= 100
 
-def get_location():
-    data = requests.get("https://ipinfo.io/json").json()
-    latitude, longitude = map(float, data["loc"].split(","))
+        if temperature <= 66:
+            red = 255
+        else:
+            red = 329.698727446 * ((temperature - 60) ** -0.1332047592)
+            red = clamp(red, 0, 255)
 
-    return latitude, longitude
+        if temperature <= 66:
+            green = 99.4708025861 * math.log(temperature) - 161.1195681661
+        else:
+            green = 288.1221695283 * ((temperature - 60) ** -0.0755148492)
+            green = clamp(green, 0, 255)
+        
+        if temperature >= 66:
+            blue = 255
+        elif temperature <= 19:
+            blue = 0
+        else:
+            blue = 138.5177312231 * math.log(temperature - 10) - 305.0447927307
+            blue = clamp(blue, 0, 255)
+        
+        return (red / 255, green / 255, blue / 255)
+    
+    def get_temp_brightness(self, night_shift):
+        return 1 - self.dim_percent * math.pow(night_shift, self.brightness_gamma)
 
-# def get_temperature_shift_percentage(elevation_angle):
-#     if elevation_angle <= 0:
-#         return 1
-#     else:
-#         return 1 - math.sin(math.radians(elevation_angle))
+class NightDetector:
+    def __init__(self):
+        pass
 
-def get_night_shift(elevation_angle):
-    return 1 - 1 / (1 + math.exp(-(elevation_angle - 10) / 3))
+    def get_sun_elevation_angle(self, location: LocationInfo):
+        time = datetime.now(timezone.utc)
+        elevation_angle = 90 - zenith(location.observer, time, True)
 
-def get_temperature(night_shift):
-    return day_temp + (night_temp - day_temp) * night_shift
+        return elevation_angle
+    
+    def get_night_shift(self, location: LocationInfo):
+        elevation_angle = self.get_sun_elevation_angle(location)
 
-GAMMA_FACTOR = 2.2
+        return 1 - 1 / (1 + math.exp(-(elevation_angle - 10) / 3))
 
-def get_brightness(night_shift):
-    return 1 - night_dim_percent * math.pow(night_shift, GAMMA_FACTOR) # Gamma function
+class Display:
+    platform: str
 
-def set_display_appearance(rgbScale, brightness):
-    x11 = ctypes.cdll.LoadLibrary(ctypes.util.find_library("X11"))
-    xf86vm = ctypes.cdll.LoadLibrary(ctypes.util.find_library("Xxf86vm"))
+    xf86vm: ctypes.CDLL
+    display: int
+    screen: int
+    c_int_size: int
+    c_array_type: type[ctypes.Array[ctypes.c_ushort]]
 
-    x11.XOpenDisplay.restype = ctypes.c_void_p
-    display = x11.XOpenDisplay(None)
+    def __init__(self):
+        self.platform = platform.system()
 
-    screen = x11.XDefaultScreen(display)
+        if self.platform != "Linux":
+            raise NotImplementedError(f"Unsupported OS: {self.platform}" "Supported platforms: Linux")
+        
+        x11_library = ctypes.util.find_library("X11")
+        xf86vm_library = ctypes.util.find_library("Xxf86vm")
 
-    size = ctypes.c_int()
-    xf86vm.XF86VidModeGetGammaRampSize(display, screen, ctypes.byref(size))
+        if not x11_library:
+            raise ImportError("X11 library not found.")
+        
+        if not xf86vm_library:
+            raise ImportError("Xxf86vm library not found.")
+        
+        x11 = ctypes.cdll.LoadLibrary(x11_library)
+        xf86vm = ctypes.cdll.LoadLibrary(xf86vm_library)
 
-    n = size.value
+        x11.XOpenDisplay.restype = ctypes.c_void_p
+        display = x11.XOpenDisplay(None)
 
-    r = [int(65535 * (i / n) * rgbScale[0] * brightness) for i in range(n)]
-    g = [int(65535 * (i / n) * rgbScale[1] * brightness) for i in range(n)]
-    b = [int(65535 * (i / n) * rgbScale[2] * brightness) for i in range(n)]
+        screen = x11.XDefaultScreen(display)
 
-    array_type = ctypes.c_ushort * n
-    r_arr = array_type(*r)
-    g_arr = array_type(*g)
-    b_arr = array_type(*b)
+        size = ctypes.c_int()
+        xf86vm.XF86VidModeGetGammaRampSize(display, screen, ctypes.byref(size))
 
-    xf86vm.XF86VidModeSetGammaRamp(display, screen, n, r_arr, g_arr, b_arr)
+        c_int_size = size.value
+        c_array_type = ctypes.c_ushort * c_int_size
 
-def update():
-    current_time = datetime.now(timezone.utc)
-    latitude, longitude = get_location()
-    elevation_angle = get_sun_elevation_angle(latitude, longitude, current_time)
+        self.xf86vm = xf86vm
+        self.display = display
+        self.screen = screen
+        self.c_int_size = c_int_size
+        self.c_array_type = c_array_type
+    
+    def set_display(self, colour: tuple[float, float, float], brightness: float):
+        colour_arrs = []
 
-    night_shift = get_night_shift(elevation_angle)
-    temperature = get_temperature(night_shift)
-    rgbScale = get_rgb_from_temp(temperature)
-    brightness = get_brightness(night_shift)
+        for i in range(3):
+            val = [int(65535 * (j / self.c_int_size) * colour[i] * brightness) for j in range(self.c_int_size)]
+            arr = self.c_array_type(*val)
 
-    print(rgbScale)
+            colour_arrs.append(arr)
 
-    print(elevation_angle)
+        self.xf86vm.XF86VidModeSetGammaRamp(self.display, self.screen, self.c_int_size, colour_arrs[0], colour_arrs[1], colour_arrs[2])
 
-    set_display_appearance(rgbScale, brightness)
+class App:
+    def __init__(self):
+        display = Display()
+        display.set_display((1, 1, 1), 1)
 
-while True:
-    update()
+App()
 
-    time.sleep(60)
+# def update():
+#     current_time = datetime.now(timezone.utc)
+#     latitude, longitude = get_location()
+#     elevation_angle = get_sun_elevation_angle(latitude, longitude, current_time)
+
+#     night_shift = get_night_shift(elevation_angle)
+#     temperature = get_temperature(night_shift)
+#     rgbScale = get_rgb_from_temp(temperature)
+#     brightness = get_brightness(night_shift)
+
+#     print(rgbScale)
+
+#     print(elevation_angle)
+
+#     set_display_appearance(rgbScale, brightness)
+
+# while True:
+#     update()
+
+#     time.sleep(60)
